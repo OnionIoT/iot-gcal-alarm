14b38dc03e55abf878262a002fd6d54146dc7bc4
James Liu <liujames.q@gmail.com>
2017-04-10 12:37:23 -0400

    alarm script inclusion

---
+++ alarm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gpioctl dirout-high 13
+sleep 60
+gpioctl dirout-low 13
+
+
--- iotGcalAlarm.py
+++ iotGcalAlarm.py
@@ -1,8 +1,7 @@
 import os
 import json
 import urllib3
-import datetime
-import time
+from crontab import CronTab
 from icalendar import Calendar, Event, vDDDTypes
 
 
@@ -19,9 +18,40 @@ if __name__ == '__main__':
     # pull out the data string from the returned response
     rawIcs = http.request('GET', config['icalAddr']).data
 
+    # converting the calendar to readable format
     cal = Calendar.from_ical(rawIcs)
+    print cal
+
+
+    # cron = CronTab(user='root')
 
     for event in cal.walk() :
-        t = event.get('dtstart')
-        if (t is not None):
-            print t.from_ical(t)
+        timeRaw = event.get ('dtstart')
+        summary = event.get ('summary')
+
+        # this variable will contain recurrance rules of the event
+        recurrance = event.get ('rrule')
+        print summary
+
+        # the keyword used to detect which events need a buzzer is 'SET_BUZZER'
+        if (summary is not None):
+            if (summary.find ('SET_BUZZER') is not -1):
+                t = timeRaw.split (' ')
+
+                date = t[0].split ('-')
+                month = int (date[1])
+                day = int (date[2])
+
+                time = t[1].split ('+')
+                time = time.split (':')
+                hour = int (time[0])
+                minute = int (time[1])
+
+            #     job = cron.new (command='/usr/bin/echo gpioctl dirout-high 13')
+            #     job.month.on (month)
+            #     job.day.on (day)
+            #     job.hour.on (hour)
+            #     job.minute.on (minute)
+            #     job.write()
+
+
